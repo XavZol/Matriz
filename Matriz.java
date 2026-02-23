294eee69736758373897eac2bea41ca368190693
XavZol <alansolis707@gmail.com>
2026-02-23 16:01:38 -0600

    Transponer una matriz 5x9.

--- Matriz.java
+++ Matriz.java
@@ -1,49 +1,42 @@
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
-/* Crear y cargar una matriz de tamaño n x m, mostrar la suma de cada fila y de cada columna. */
+/* Utilizando dos matrices de tamaño 5x9 y 9x5, cargar la primera y trasponerla en la segunda. */
 public class Matriz {
     
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int matriz[][], nFilas,nCol, sumaFilas, sumaCol;
+        int matriz1[][], matriz2[][];
 
-        nFilas = Integer.parseInt(JOptionPane.showInputDialog("Digite el número de filas: "));
-        nCol = Integer.parseInt(JOptionPane.showInputDialog("Digite el número de columnas: "));
+        matriz1 = new int[5][9];
+        matriz2 = new int[9][5];
 
-        matriz = new int[nFilas][nCol];
-
-        System.out.println("Digite la matriz");
-        for(int i=0; i<nFilas; i++) {
-            for(int j=0; j<nCol; j++) {
+        System.out.println("Digite la matriz: ");
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<9; j++) {
                 System.out.print("Matriz["+i+"] ["+j+"]: ");
-                matriz[i][j] = entrada.nextInt();
+                matriz1[i][j] = entrada.nextInt();
             }
         }
-        System.out.println("\nla matriz es: ");
-        for(int i=0; i<nFilas; i++) {
-            for(int j=0; j<nCol; j++) {
-                System.out.print(matriz[i][j]+" ");
+        System.out.println("\nLa matriz original es: ");
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<9; j++) {
+                System.out.print(matriz1[i][j]+ " ");
             }
             System.out.println("");
         }
-        // SUMA de las filas 
-        for(int i=0; i<nFilas; i++) {
-            sumaFilas = 0;
-            for(int j=0; j<nCol; j++) {
-                sumaFilas += matriz[i][j];
+
+        // Trasponemos
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<9; j++) {
+                matriz2[j][i] = matriz1[i][j];
             }
-            System.out.print("\nLa suma de la fila["+i+"] es: "+sumaFilas);
         }
-        System.out.println("");
-        // SUMA de las columnas
-        for(int j=0; j<nCol; j++) {
-            sumaCol = 0;
-            for(int i=0; i<nFilas; i++) {
-                sumaCol += matriz[i][j];
+        System.out.println("\nLa matriz traspuesta es: ");
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<5; j++) {
+                System.out.print(matriz2[i][j]+" ");
             }
-            System.out.print("\nLa suma de la columna["+j+"] es: "+sumaCol);
+            System.out.println("");
         }
-        System.out.print("");
     }
 }
