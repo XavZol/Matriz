f65afd8540903a3d798e9b891a681ea371368bfc
XavZol <alansolis707@gmail.com>
2026-02-23 14:40:15 -0600

    Primer Matriz en Java.

---
+++ Matriz.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Matriz {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int matriz[][], nFilas, nCol;
+
+        nFilas = Integer.parseInt(JOptionPane.showInputDialog("Digite el número de filas: "));
+        nCol = Integer.parseInt(JOptionPane.showInputDialog("Digite el número de columnas: "));
+
+        matriz = new int[nFilas][nCol];
+
+        System.out.println("Digite la matriz: ");
+        for(int i=0; i<nFilas; i++) {
+            for(int j=0; j<nCol; j++) {
+                System.out.print("Matriz ["+i+"] ["+j+"]: ");
+                matriz[i][j] = entrada.nextInt();
+            }
+        }
+        System.out.println("\nLa matriz es: ");
+        for(int i=0; i<nFilas; i++) { // número de filas
+            for(int j=0; j<nCol; j++) { // número de columnas
+                System.out.print(matriz[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
